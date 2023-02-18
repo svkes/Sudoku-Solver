46366403f390058000ebb7c487fd2988bc33c034
Tai Koeda <gwckoeda@gmail.com>
2023-02-18 15:05:41 -0500

    implemented input function

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
+import javax.print.attribute.standard.Sides;
+
+import java.io.File;
+import java.io.FileNotFoundException;  // Import this class to handle errors
+
+
 public class Sudoku {
-    public static int[][] GRID_TO_SOLVE = {
+    /*public static int[][] GRID_TO_SOLVE = {
             {9,0,0,1,0,0,0,0,5},
             {0,0,5,0,9,0,2,0,1},
             {8,0,0,0,4,0,0,0,0},
@@ -9,7 +17,9 @@ public class Sudoku {
             {2,0,0,3,0,0,0,0,6},
             {0,0,0,2,0,0,9,0,0},
             {0,0,1,9,0,4,5,7,0},
-    };
+    };*/
+    public static int[][] GRID_TO_SOLVE = new int[8][8];
+
 
     private final int[][] board;
     public static final int EMPTY = 0; // empty cell
@@ -86,6 +96,10 @@ public class Sudoku {
     }
 
     public static void main(String[] args) {
+        if (!loadSudoku()) {
+            System.out.println("Somethig went wrong");
+        }
+        else {
         Sudoku sudoku = new Sudoku(GRID_TO_SOLVE);
         System.out.println();
         sudoku.display();
@@ -96,5 +110,32 @@ public class Sudoku {
         } else {
             System.out.println("Sudoku Is Unsolvable");
         }
+        }   
+    }
+    public static boolean loadSudoku()
+    {
+        try
+        {
+            Scanner scan = new Scanner(new File("/Users/taikoeda/Desktop/CS Project/Sudoku Solver/src/board.txt"));
+
+            if (!scan.hasNextLine())    {System.out.println("Empty File. Exiting..."); return false;}
+
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    if(scan.hasNextInt()) {
+                        GRID_TO_SOLVE[i][j] = scan.nextInt();
+                    }
+                }
+            }
+            scan.close(); 
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File Not Found. Please create a puzzle.txt in the same folder this app is in. Exiting...");
+        }
+        System.out.println("Sudoku is now loaded.");
+        return true;
     }
 }
